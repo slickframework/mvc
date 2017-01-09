b300e70a47e4104870b2ddef6dede1ddd665ccfd
Filipe Silva <silvam.filipe@gmail.com>
2017-01-09 00:32:43 -0100

    Map builder insterface

---
+++ src/Http/Router/RouteBuilderInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of slick/mvc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Slick\Mvc\Http\Router;
+
+use Aura\Router\Map;
+
+/**
+ * Router Builder Interface
+ *
+ * @package Slick\Mvc\Http\Router
+ * @author  Filipe Silva <silvam.filipe@gmail.com>
+ */
+interface RouteBuilderInterface
+{
+
+    /**
+     * Map builder handler
+     *
+     * @see http://auraphp.com/packages/3.x/Router/custom-maps.html#1-4-5
+     *
+     * @param Map $map
+     *
+     * @return self|RouteBuilderInterface
+     */
+    public function build(Map $map);
+}
