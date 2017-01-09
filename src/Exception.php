2cad417391f8d28d3dc05aa819fbce4d75235103
Filipe Silva <filipe.silva@sata.pt>
2017-01-09 19:12:30 -0100

    Started route builder.

---
+++ src/Exception.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of slick/mvc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Slick\Mvc;
+
+/**
+ * Slick/Mvc generic exception
+ *
+ * @package Slick\Mvc
+ * @author  Filipe Silva <filipe.silva@sata.pt>
+ */
+interface Exception
+{
+
+}
---
+++ src/Exception/RoutesFileParserException.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of slick/mvc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Slick\Mvc\Exception;
+
+use RuntimeException;
+use Slick\Mvc\Exception;
+
+/**
+ * Routes File Parser Exception
+ *
+ * @package Slick\Mvc\Exception
+ * @author  Filipe Silva <filipe.silva@sata.pt>
+ */
+class RoutesFileParserException extends RuntimeException implements Exception
+{
+
+}
